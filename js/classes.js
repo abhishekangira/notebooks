2306f4e4fb20a7508280dadabf3c3169fc9ada4c
Abhishek Angira <abhishekspy007@gmail.com>
2020-04-30 17:01:56 +0530

    formatting

--- js/classes.js
+++ js/classes.js
@@ -1,44 +1,44 @@
 // CONFIGURATIONS
 function prt(text) {
-    console.log(text);
+  console.log(text);
 }
 // END CONFIGURATIONS
 
 class Chapter {
-    constructor(name, text) {
-        this.name = name;
-        this.text = text;
-    }
+  constructor(name, text) {
+    this.name = name;
+    this.text = text;
+  }
 
-    print() {
-        prt(this.name, this.text);
-    }
+  print() {
+    prt(this.name, this.text);
+  }
 }
 
 export default class Notebook {
-    constructor(name) {
-        this.name = name;
-        this.chapters = [];
-    }
+  constructor(name) {
+    this.name = name;
+    this.chapters = [];
+  }
 
-    addChapter(name, text) {
-        let chapter = new Chapter(name, text);
-        this.chapters.push(chapter);
-        prt('Chapter added!');
-    }
+  addChapter(name, text) {
+    let chapter = new Chapter(name, text);
+    this.chapters.push(chapter);
+    prt("Chapter added!");
+  }
 
-    openChapter(num) {
-        if (this.chapters[num - 1]) prt(this.chapters[num - 1].text);
-        else prt('Opps! This page number doesn\'t exist!')
-    }
+  openChapter(num) {
+    if (this.chapters[num - 1]) prt(this.chapters[num - 1].text);
+    else prt("Opps! This page number doesn't exist!");
+  }
 
-    showIndex() {
-        this.chapters.forEach((v, i) => prt(`${i + 1}. ${v.name}: ${v.text}`));
-    }
+  showIndex() {
+    this.chapters.forEach((v, i) => prt(`${i + 1}. ${v.name}: ${v.text}`));
+  }
 
-    print() {
-        for (let x of this.chapters) {
-            x.print();
-        }
+  print() {
+    for (let x of this.chapters) {
+      x.print();
     }
+  }
 }
--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import Notebook from './classes.js'
+import Notebook from "./classes.js";
 
 // if (!localStorage.showNotebooks) {
 //     localStorage.showNotebooks = 0;
@@ -10,13 +10,11 @@ import Notebook from './classes.js'
 //     return arr;
 // }
 
-
 // const newNotebookName;
 // editedNotebookName: '',
 // showNotebooks: Boolean(Number(localStorage.showNotebooks)),
 // showEditOptions: showEditArr(),
 // notebooks: notesArr()
-
 
 // deleteNotebook(event) {
 //     let arr = notesArr();
@@ -55,7 +53,7 @@ import Notebook from './classes.js'
 /******************* LocalStorage Initialization ***********************/
 
 if (!localStorage.notebooks) {
-    localStorage.notebooks = '';
+  localStorage.notebooks = "";
 }
 
 /******************* Variables & Constants ***********************/
@@ -63,142 +61,125 @@ if (!localStorage.notebooks) {
 const PATH = window.location.pathname;
 const CREATE = "/pages/new-note.html";
 const HOME = "/index.html";
-const newnoteInput = document.getElementById('inp-create');
-const noNotes = document.getElementById('no-notes');
-const notesList = document.getElementById('notes-list');
-const btnCreate = document.getElementById('btn-create');
-const btnCreateHome = document.getElementById('btn-create-home');
-const btnDeleteAll = document.getElementById('btn-delete-all');
-const modalDeleteAll = document.getElementById('modal-delete-all');
-const modalAddChapter = document.getElementById('modal-add-chapter');
-const modalEmptyNoteName = document.getElementById('modal-empty-note-name');
-const modalYes = document.getElementsByClassName('modal-yes')[0];
-const modalNo = document.getElementsByClassName('modal-no')[0];
-const modalOk = document.getElementsByClassName('modal-ok')[0];
+const newnoteInput = document.getElementById("inp-create");
+const noNotes = document.getElementById("no-notes");
+const notesList = document.getElementById("notes-list");
+const btnCreate = document.getElementById("btn-create");
+const btnCreateHome = document.getElementById("btn-create-home");
+const btnDeleteAll = document.getElementById("btn-delete-all");
+const modalDeleteAll = document.getElementById("modal-delete-all");
+const modalAddChapter = document.getElementById("modal-add-chapter");
+const modalEmptyNoteName = document.getElementById("modal-empty-note-name");
+const modalYes = document.getElementsByClassName("modal-yes")[0];
+const modalNo = document.getElementsByClassName("modal-no")[0];
+const modalOk = document.getElementsByClassName("modal-ok")[0];
 let notes = notesArr().join();
 
 /******************* Path Specific Code ***********************/
 
-document.getElementsByTagName('main')[0].style.opacity = '1';
-if (PATH == HOME || PATH == '/') {
-    updateNotesList();
-    if (notes) {
-        noNotes.style.display = 'none';
-        notesList.style.display = 'grid';
-    } else {
-        noNotes.style.display = 'flex';
-        notesList.style.display = 'none';
-        btnCreateHome.style.display = 'none';
-        btnDeleteAll.style.display = 'none';
-    }
-    btnDeleteAll.addEventListener("click", () => displayModal(modalDeleteAll));
-    document.addEventListener("keydown", keyShorcuts);
+document.getElementsByTagName("main")[0].style.opacity = "1";
+if (PATH == HOME || PATH == "/") {
+  updateNotesList();
+  if (notes) {
+    noNotes.style.display = "none";
+    notesList.style.display = "grid";
+  } else {
+    noNotes.style.display = "flex";
+    notesList.style.display = "none";
+    btnCreateHome.style.display = "none";
+    btnDeleteAll.style.display = "none";
+  }
+  btnDeleteAll.addEventListener("click", () => displayModal(modalDeleteAll));
+  document.addEventListener("keydown", keyShorcuts);
 } else if (PATH == CREATE) {
-    btnCreate.addEventListener("click", () => {
-        if (newnoteInput.value)
-            displayModal(modalAddChapter);
-        else
-            displayModal(modalEmptyNoteName);
-    });
-    newnoteInput.addEventListener("keypress", addNoteOnEnter);
+  btnCreate.addEventListener("click", () => {
+    if (newnoteInput.value) displayModal(modalAddChapter);
+    else displayModal(modalEmptyNoteName);
+  });
+  newnoteInput.addEventListener("keypress", addNoteOnEnter);
 }
 
 /************* Functions **************/
 
 function clearStorage() {
-    localStorage.clear();
-    localStorage.notebooks = '';
+  localStorage.clear();
+  localStorage.notebooks = "";
 }
 
 function addNote() {
-    let n = new Notebook(newnoteInput.value);
-    if (!localStorage.notebooks) {
-        localStorage.notebooks = n.name;
-    } else {
-        localStorage.notebooks = localStorage.notebooks + `,${n.name}`;
-    }
+  let n = new Notebook(newnoteInput.value);
+  if (!localStorage.notebooks) {
+    localStorage.notebooks = n.name;
+  } else {
+    localStorage.notebooks = localStorage.notebooks + `,${n.name}`;
+  }
 }
 
 function notesArr() {
-    return localStorage.notebooks.split(',');
+  return localStorage.notebooks.split(",");
 }
 
 function addNoteOnEnter(e) {
-    if (e.target.id == 'btn-create' || e.keyCode == 13) {
-        btnCreate.click();
-    }
+  if (e.target.id == "btn-create" || e.keyCode == 13) {
+    btnCreate.click();
+  }
 }
 
 function keyShorcuts(e) {
-    if (e.code == 'KeyN' && e.shiftKey) {
-        btnCreateHome.click();
-    } else if (e.code == 'KeyD' && e.shiftKey) {
-        btnDeleteAll.click();
-    }
+  if (e.code == "KeyN" && e.shiftKey) {
+    btnCreateHome.click();
+  } else if (e.code == "KeyD" && e.shiftKey) {
+    btnDeleteAll.click();
+  }
 }
 
 function updateNotesList() {
-    notesArr().forEach((v, i) => {
-        let div = document.createElement("div");
-        div.className = 'note';
-        div.textContent = v;
-        notesList.appendChild(div);
-    })
+  notesArr().forEach((v, i) => {
+    let div = document.createElement("div");
+    div.className = "note";
+    div.textContent = v;
+    notesList.appendChild(div);
+  });
 }
 
 function displayModal(modal) {
-    let elList = '.modal-box, .modal-box .btnWarn, .modal-box p, .modal-box div';
-    document.body.style.overflow = 'hidden';
-    modal.style.visibility = 'visible';
-    modal.style.display = 'flex';
-    modal.style.opacity = '1';
+  let elList = ".modal-box, .modal-box .btnWarn, .modal-box p, .modal-box div";
+  document.body.style.overflow = "hidden";
+  modal.style.visibility = "visible";
+  modal.style.display = "flex";
+  modal.style.opacity = "1";
 
-    modal.addEventListener("click", (e) => {
-        if (!e.target.matches(elList)) {
-            modal.style.visibility = 'hidden'
-            modal.style.opacity = '0'
-        }
+  modal.addEventListener("click", (e) => {
+    if (!e.target.matches(elList)) {
+      modal.style.visibility = "hidden";
+      modal.style.opacity = "0";
+    }
+  });
+
+  modalYes.addEventListener("click", () => {
+    if (PATH == HOME || PATH == "/") {
+      clearStorage();
+    } else if (PATH == CREATE) {
+      console.log("he wants a new chapter!");
+    }
+    modal.style.visibility = "hidden";
+    modal.style.opacity = "0";
+    document.body.style.overflow = null;
+  });
+
+  modalNo.addEventListener("click", (e) => {
+    if (PATH == CREATE) {
+      addNote();
+    }
+    modal.style.visibility = "hidden";
+    modal.style.opacity = "0";
+    document.body.style.overflow = null;
+  });
+
+  if (PATH == CREATE)
+    modalOk.addEventListener("click", (e) => {
+      modal.style.visibility = "hidden";
+      modal.style.opacity = "0";
+      document.body.style.overflow = null;
     });
-
-    modalYes.addEventListener("click", () => {
-        if (PATH == HOME || PATH == '/') {
-            clearStorage();
-        } else if (PATH == CREATE) {
-            console.log('he wants a new chapter!')
-        }
-        modal.style.visibility = 'hidden';
-        modal.style.opacity = '0'
-        document.body.style.overflow = null;
-    });
-
-    modalNo.addEventListener("click", (e) => {
-        if (PATH == CREATE) {
-            addNote();
-        }
-        modal.style.visibility = 'hidden';
-        modal.style.opacity = '0'
-        document.body.style.overflow = null;
-    });
-
-    if (PATH == CREATE)
-        modalOk.addEventListener("click", (e) => {
-            modal.style.visibility = 'hidden';
-            modal.style.opacity = '0'
-            document.body.style.overflow = null;
-        });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
